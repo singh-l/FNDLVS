3b18ebc42a96ca00f6cba384eed19b9daf61c7df
Lovedeep <singhl.lovedeep@gmail.com>
2020-11-26 15:38:46 +0530

    adding ANN versions

---
+++ models/ANN/DOC2VEC/getEmbeddings.py
@@ -0,0 +1,69 @@
+#this is the function to get doc2vec embeddings of the given file
+
+#make the necessary imports
+import numpy as np
+import re
+import string
+import pandas as pd
+from gensim.models import Doc2Vec
+from gensim.models.doc2vec import LabeledSentence
+from gensim import utils
+from nltk.corpus import stopwords
+
+
+#clean the text by removing useless symbols and stopwords
+
+def textClean(text):
+    text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
+    text = text.lower().split()
+    stops = set(stopwords.words("english"))
+    text = [w for w in text if not w in stops]
+    text = " ".join(text)
+    return (text)
+
+
+def cleanup(text):
+    text = textClean(text)
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text
+
+
+#construct labeled sentences
+
+def constructLabeledSentences(data):
+    sentences = []
+    for index, row in data.iteritems():
+        sentences.append(LabeledSentence(utils.to_unicode(row).split(), ['Text' + '_%s' % str(index)]))
+    return sentences
+
+#run the shallow 3 layer nueral net and get doc2vec embeddings
+
+def getEmbeddings(path,vector_dimension=300):
+    data = pd.read_csv(path)
+
+    missing_rows = []
+    for i in range(len(data)):
+        if data.loc[i, 'statement'] != data.loc[i, 'statement']:
+            missing_rows.append(i)
+    data = data.drop(missing_rows).reset_index().drop(['index','id'],axis=1)
+
+    for i in range(len(data)):
+        data.loc[i, 'statement'] = cleanup(data.loc[i,'statement'])
+
+    x = constructLabeledSentences(data['statement'])
+    y = data['label'].values
+
+    text_model = Doc2Vec(min_count=1, window=5, vector_size=vector_dimension, sample=1e-4, negative=5, workers=7, epochs=10,
+                         seed=1)
+    text_model.build_vocab(x)
+    text_model.train(x, total_examples=text_model.corpus_count, epochs=text_model.iter)
+
+
+    text_arrays = np.zeros((len(x), vector_dimension))
+    labels_array = np.zeros(len(x))
+    
+    for i in range(len(x)):
+        text_arrays[i] = text_model.docvecs['Text_' + str(i)]
+        labels_array[i] = y[i]
+
+    return text_arrays, labels_array
